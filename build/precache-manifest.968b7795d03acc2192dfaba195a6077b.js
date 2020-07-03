98f77fcf78199df1359e296806175ab0d9727846
AkikJana <akik.e.aj@gmail.com>
2020-07-03 16:02:56 +0530

    hm

---
+++ build/precache-manifest.968b7795d03acc2192dfaba195a6077b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4f5bf2812d0e559715110d2e22e289",
+    "url": "/1705205/index.html"
+  },
+  {
+    "revision": "63104d8050ff3b8ba930",
+    "url": "/1705205/static/css/main.e5e7677d.chunk.css"
+  },
+  {
+    "revision": "23564eb384f8b59bf3ae",
+    "url": "/1705205/static/js/2.320abf92.chunk.js"
+  },
+  {
+    "revision": "38a4b558739197dd9ec900c0561b7e3e",
+    "url": "/1705205/static/js/2.320abf92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63104d8050ff3b8ba930",
+    "url": "/1705205/static/js/main.718b0dfa.chunk.js"
+  },
+  {
+    "revision": "02c48c66713b08bc2c44",
+    "url": "/1705205/static/js/runtime-main.c325482f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/1705205/static/media/logo.ee7cd8ed.svg"
+  }
+]);
